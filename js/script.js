8fdc111c2ce47de3c5ccbffd43ae5c880631eec5
DanU-coder <danielurb453@gmail.com>
2024-10-18 10:31:19 -0600

    get on top button

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let fibonacci = () =>{
+    let x = 0, y = 1;
+    for(i = 0; i < 10; i++){
+        z = (x + y)
+        console.log(z)
+        x = y; y = z;
+    }
+}
+fibonacci()
+
+let fibonaccDosVariables = () =>{
+    let x = 0, y = 1;
+}
+
+fibonaccDosVariables()
